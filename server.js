2901d0b87078dff93dff22f1d5813cbadbccae5b
kentquirk <kentquirk@gmail.com>
2013-08-15 20:27:33 -0700

    see if it works on 8009

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(80);
+}).listen(8009);
 console.log('Server running.');
